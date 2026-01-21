df7b14387e6d3d0bc36792057771d37683922f9a
ANdroxee <anto.dongay123@gmail.com>
2026-01-21 16:36:13 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Dev environment for Early (React Native/Expo)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            yarn
+            watchman # Important pour React Native
+          ];
+
+          shellHook = ''
+            echo "⚛️ Environnement React Native (Expo) prêt !"
+            echo "Node version: $(node -v)"
+          '';
+        };
+      }
+    );
+}
